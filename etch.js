aaa8457acb0c0ca0ced1eee962b3709f0a109ab9
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-02-17 12:16:28 +0000

    Make 16 x 16 grid using JavaScript. Adjust size of container to make it neat

--- etch.js
+++ etch.js
@@ -1,1 +1,9 @@
-document.createElement(div);
+const container = document.querySelector('.container');
+let smallDiv; 
+
+for (let i = 1; i <= 256; i++)
+  {
+  smallDiv = document.createElement('div');
+  smallDiv.classList.add('griddiv');
+  container.appendChild(smallDiv);
+  }
