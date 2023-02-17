3bef7b0b22734d771479f15ab178cd0a92dd5148
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-02-17 15:09:21 +0000

    Prompt box only accepts number between 2 and 100 and then wipes previous grid

--- etch.js
+++ etch.js
@@ -1,5 +1,10 @@
 const container = document.querySelector('.container');
 let smallDiv; 
+let num;
+
+function whenHover(div) {
+    div.style.backgroundColor = 'red';
+}
 
 for (let i = 1; i <= 256; i++)
   {
@@ -7,3 +12,19 @@ for (let i = 1; i <= 256; i++)
   smallDiv.classList.add('griddiv');
   container.appendChild(smallDiv);
   }
+const squares = document.querySelectorAll('.griddiv');
+  squares.forEach((div) => div.addEventListener('mouseover', () => {div.style.backgroundColor = 'red'}));
+
+const topButton = document.querySelector('.topbutton');
+console.log(topButton);
+topButton.addEventListener('click', ohBoy);
+
+function ohBoy() {
+    num = Number(prompt('Choose a side length for the new grid'));
+    let i = 0;
+    while (((num < 1) || (num > 100) || ((Math.floor(num) - num) !== 0)) && (i++ <= 4))
+       {num = Number(prompt('Please choose a whole number between 2 and 100 inclusive.'))};
+    let allDivs = document.querySelectorAll('.griddiv');
+    allDivs.forEach((div) => div.remove());
+}
+
